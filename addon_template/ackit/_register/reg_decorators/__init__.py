5e054410c5c6948c8c9e14ae0ec93a064fca55a9
jfranmatheu <45881831+jfranmatheu@users.noreply.github.com>
2023-05-30 13:17:16 +0200

    add handler registration decorator

--- addon_template/ackit/_register/reg_decorators/__init__.py
+++ addon_template/ackit/_register/reg_decorators/__init__.py
@@ -2,10 +2,12 @@ from .reg_property_group import PropertyGroupRegister
 from .reg_ui_append import UIAppend
 from .reg_rna_sub import subscribe_to_rna_change # RNASubscription
 from .reg_timer import new_timer_as_decorator
+from .reg_handlers import Handlers
 
 
 class RegDeco:
     PROP_GROUP = PropertyGroupRegister
     RNA_SUB = subscribe_to_rna_change
+    HANDLER = Handlers
     TIMER = new_timer_as_decorator
     UI_APPEND = UIAppend
