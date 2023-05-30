8f0905a44791dc47b4b175e2e9ab9ca31a969198
jfranmatheu <45881831+jfranmatheu@users.noreply.github.com>
2023-05-30 13:36:42 +0200

    add panel popover example

--- ackit_examples/panel.py
+++ ackit_examples/panel.py
@@ -5,7 +5,7 @@ from addon_template.ackit import Reg, SpaceType
 from .operator import NiceOperator
 
 
-@Reg.Flag.Panel.HIDE_HEADER
+@Reg.Flag.Panel.HIDE_HEADER # Optional Flag to hide the header of the panel.
 class NicePanel(Reg.Type.UI.PANEL):
     space_type = SpaceType.VIEW_3D
     tab = 'Nice'
@@ -14,3 +14,12 @@ class NicePanel(Reg.Type.UI.PANEL):
     def draw_ui(self, context: Context, layout: UILayout):
         layout.label(text="Hello World!", icon='MONKEY')
         NiceOperator.draw_in_layout(layout, label="Action!")
+
+
+class NicePopoverPanel(Reg.Type.UI.POPOVER):
+    space_type = SpaceType.VIEW_3D
+    label = 'Popover Panel'
+
+    def draw_ui(self, context: Context, layout: UILayout):
+        layout.label(text="Popover Panel Content!")
+        NiceOperator.draw_in_layout(layout, label="Action! :-)")
